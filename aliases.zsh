2976d508b4fb9a61c2941112fa0d3d1e70d78159
Luiz Claudio Moreira Junior <luizclaudiomoreira@gmail.com>
2023-05-05 11:10:55 +0100

    ...

--- aliases.zsh
+++ aliases.zsh
@@ -2,13 +2,26 @@ if [ -f /usr/bin/dircolors ]; then
   LS_OPTS="--color=auto"
 fi
 
+alias lab="cd ~/Code/luizclaudiomoreira/lab && clear"
+
+alias g=git
+alias irb="clear && irb"
+alias vim="nvim"
+# alias ruby="clear && ruby"
+
+alias a="cd ~/Code/programador-do-futuro/aulas && clear"
+alias e="cd ~/Code/luizclaudiomoreira/exercicios-ruby && clear"
+alias ep="cd ~/Code/programador-do-futuro/exercicios-ruby && clear"
+alias lc="cd ~/Code/luizclaudiomoreira && clear"
+alias t="cd ~/Code/programador-do-futuro/t0-exercicios && clear"
+
 alias brew="arch -arm64 /opt/homebrew/bin/brew"
 alias ibrew="arch -x86_64 /usr/local/bin/brew"
 
 # generic
-alias t="tmux"
+# alias t="tmux"
 alias tn="tmux new-session -A -D -s"
-alias v="vagrant"
+# alias v="vagrant"
 alias ls="ls -lh $LS_OPTS"
 alias grep="grep --color=auto"
 
@@ -17,23 +30,24 @@ alias ae="vim ~/.dotfiles/aliases.zsh"
 alias ar="source ~/.dotfiles/aliases.zsh"
 
 # docker
-alias dk="docker"
-alias dkc="docker compose"
+# alias dk="docker"
+# alias dkc="docker compose"
 
 # bundle
-alias bi="bundle install"
-alias bu="bundle update"
+# alias bi="bundle install"
+# alias bu="bundle update"
 
 # rake / rails / rspec / rubocop
+alias rspec="clear && rspec"
 alias rk="rake"
 alias ra="rails"
-alias ru="rubocop"
+# alias ru="rubocop --require rubocop-rspec --require rubocop-rails"
 alias rs="clear && rspec --format=documentation"
 
 alias dbreset="rails db:environment:set RAILS_ENV=test && RAILS_ENV=test rails db:drop && RAILS_ENV=test rails db:create && RAILS_ENV=test rails db:migrate && RAILS_ENV=development rails db:reset"
 
 # spring
-alias sp="spring"
-alias sra="spring rails"
-alias sps="clear && spring stop && ps -ax | grep spring"
-alias srs="clear && spring rspec --format=documentation"
+# alias sp="spring"
+# alias sra="spring rails"
+# alias sps="clear && spring stop && ps -ax | grep spring"
+# alias srs="clear && spring rspec --format=documentation"
--- symlinks/hammerspoon/init.lua
+++ symlinks/hammerspoon/init.lua
@@ -1,3 +1,14 @@
-hs.loadSpoon("SpoonInstall")
+hs.loadSpoon('SpoonInstall')
+spoon.SpoonInstall.use_syncinstall = true
+spoon.SpoonInstall:andUse('SpoonInstall')
+spoon.SpoonInstall:andUse('MyWindowManager')
+spoon.SpoonInstall:andUse('WindowHalfsAndThirds', { hotkeys = 'default' })
 
-spoon.SpoonInstall:andUse("WindowHalfsAndThirds", { hotkeys = 'default' })
+hs.hotkey.bindSpec({ { 'cmd', 'alt', 'ctrl' }, 'R' }, function()
+  hs.reload()
+end)
+
+hs.hotkey.bindSpec({ { 'cmd', 'alt', 'ctrl' }, 'H' }, function()
+  hs.toggleConsole()
+end)
+
